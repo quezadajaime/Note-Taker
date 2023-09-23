fc3408bca32479451d46fbad010d63e41fb5aeb8
Jaime <quezadajaime@ymail.com>
2023-09-23 12:31:08 -0700

    wrote html and api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const db = require('../db/db.json');
+const router = require('express').Router();
+
+router.get('/api/notes', (req, res) => {
+    fs.readFile('.db/db.json', (err, data) => {
+        if (err) throw err;
+        console.log(JSON.parse(data));
+
+        res.send(data)
+    })
+})
+
+router.post('/api/notes', (req, res) => {
+    let newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+
+        newData.push(newNote);
+        console.log(newData)
+
+        fs.writeFile('../db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+
+            res.send('successfully added');
+        })
+    });
+
+})
+
+module.exports = router;
