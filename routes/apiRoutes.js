358a64b454df7dd2b499939ac7184c911a8fcc62
Jaime <quezadajaime@ymail.com>
2023-09-24 15:54:19 -0700

    local host working on insomnia

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-
 const db = require('../db/db.json');
 const router = require('express').Router();
+const uniqid = require('uniqid');
 
 router.get('/api/notes', (req, res) => {
-    fs.readFile('.db/db.json', (err, data) => {
+    fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
         console.log(JSON.parse(data));
 
@@ -25,7 +25,7 @@ router.post('/api/notes', (req, res) => {
         newData.push(newNote);
         console.log(newData)
 
-        fs.writeFile('../db/db.json', JSON.stringify(newData), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
             if (err) throw err;
 
             res.send('successfully added');
